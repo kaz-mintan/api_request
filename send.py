541aff5b742dce52afa711f4fa0d80823a066483
kazumi kumagai <kazumi.kumagai@riken.jp>
2021-02-01 00:14:51 -0800

    updated

--- send.py
+++ send.py
@@ -8,13 +8,12 @@ url = "https://api-sunaba.xaiml.docomo-dialog.com/dialogue"
 
 # APIに送信する情報
 headers = {"Content-Type": "application/json;charset=utf-8"}
-headers2 = {"x-api-key": "thSwyFIFfd1tHQ1PgDSv38LFqXfg7rMU7BEzDAOd", "Content-Type": "application/json;charset=utf-8"}
 
 body = {"language": "ja-JP", "botId": "KATARAI_Chatting", "appId": "705f15b5-d457-4782-b198-59da2757def0", "voiceText": "こんにちは", "clientData": { "option": { "nickname": "太郎", "nicknameY": "たろう", "sex": "男", "bloodtype": "A", "birthdateY": "1989", "birthdateM": "1", "birthdateD": "1", "age": "31", "constellations": "山羊座", "place": "東京" } }, "appRecvTime": "2017-01-01 12:00:00", "appSendTime": "2017-01-01 12:01:00"}
 
 body_account = {
-  "accountName": "riken.kazumi@gmail.com",
-  "password": "numone2019"
+  "accountName":**,
+  "password":**
 }
 
 body3 = {
@@ -27,6 +26,7 @@ body3 = {
 }
 
 # API接続の実行
-requests.post(url_login,params=body_account,headers=headers)
-result = requests.post(url, params=body, headers=headers2)
-print(result.json())
+result = requests.post(url_login,params=body_account,headers=headers)
+#result = requests.post(url, params=body, headers=headers2)
+#result = requests.post(url, params=body, headers=headers2)
+print(result)
